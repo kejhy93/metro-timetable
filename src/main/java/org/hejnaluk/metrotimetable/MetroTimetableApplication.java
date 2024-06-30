6d6d32a911b9d311154b2c069b5e3b9dd685f838
Lukáš Hejnal <kejhy93@gmail.com>
2024-06-30 07:35:28 +0000

    Add first class

---
+++ src/main/java/org/hejnaluk/metrotimetable/MetroTimetableApplication.java
@@ -0,0 +1,13 @@
+package org.hejnaluk.metrotimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroTimetableApplication.class, args);
+	}
+
+}
