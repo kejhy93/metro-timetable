1a6c756150efe68e1876f8965591a22a6fdfd3ee
hejnaluk <kejhy93@gmail.com>
2025-04-12 15:21:54 +0200

    Make retrieving blocking so all data are downloaded and processed

--- src/main/java/org/hejnaluk/metrotimetable/client/PIDClient.java
+++ src/main/java/org/hejnaluk/metrotimetable/client/PIDClient.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipEntry;
@@ -39,57 +41,67 @@ public class PIDClient {
     }
 
     public void getData() {
-        log.info("PIDClient");
+        log.info("PIDClient address is: {}", pathTOFile);
+
+        // REACTIVE
         // Use WebClient to download the ZIP file as a byte array
         webClient
                 .get()
                 .header(HttpHeaders.ACCEPT, "application/zip")
                 .retrieve()
                 .bodyToMono(byte[].class)
+                .checkpoint("After bodyToMono")
+                .doOnNext(bytes -> log.info("Received ZIP file of size: {}", bytes.length))
                 .flatMap(this::extractZipInMemory)
+                .checkpoint("After extractZipInMemory")
+                .doOnSuccess(v -> log.info("ZIP extraction completed"))
                 .then(Mono.just("ZIP file downloaded and extracted successfully in memory!"))
                 .onErrorResume(e -> {
                     log.error("Failed to download or extract ZIP", e);
                     return Mono.error(e);
-                });
+                })
+                .block();
+
+        log.info("All files are downloaded and extracted successfully in memory!");
     }
 
     // Method to extract the ZIP file in memory from a byte array
     private Mono<Void> extractZipInMemory(byte[] zipData) {
-        if (zipData == null) {
-            log.error("Zip data is null");
-            return Mono.error(new IllegalArgumentException("Zip data must not be null"));
-        }
-        try (InputStream inputStream = new java.io.ByteArrayInputStream(zipData);
-             ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
+        return Mono.<Void>create(sink -> {
+            try (InputStream inputStream = new java.io.ByteArrayInputStream(zipData);
+                 ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
+                ZipEntry entry;
+                while ((entry = zipInputStream.getNextEntry()) != null) {
+                    log.info("Extracting: {}", entry.getName());
+                    // If the entry is a file, read the content
+                    if (!entry.isDirectory()) {
+                        try (ByteArrayOutputStream fileOutputStream = new ByteArrayOutputStream()) {
+                            byte[] buffer = new byte[1024];
+                            int length;
+                            while ((length = zipInputStream.read(buffer)) > 0) {
+                                fileOutputStream.write(buffer, 0, length);
+                            }
 
-            ZipEntry entry;
-            while ((entry = zipInputStream.getNextEntry()) != null) {
-                log.info("Extracting: {}", entry.getName());
+                            // File content is now in memory
+                            byte[] fileContent = fileOutputStream.toByteArray();
+                            log.info("File size: {} bytes", fileContent.length);
 
-                // If the entry is a file, read the content
-                if (!entry.isDirectory()) {
-                    try(ByteArrayOutputStream fileOutputStream = new ByteArrayOutputStream()) {
-                        byte[] buffer = new byte[1024];
-                        int length;
-                        while ((length = zipInputStream.read(buffer)) > 0) {
-                            fileOutputStream.write(buffer, 0, length);
+                            log.info("File name is {}", entry.getName());
+                            try (FileOutputStream finalFileOutputStream = new FileOutputStream(entry.getName())) {
+                                finalFileOutputStream.write(fileContent);
+                                log.info("File created successfully.");
+                            }
                         }
-
-                        // File content is now in memory
-                        byte[] fileContent = fileOutputStream.toByteArray();
-                        log.info("File size: {} bytes", fileContent.length);
+                        // You can process the file content here (e.g., store, analyze, etc.)
                     }
-                    // You can process the file content here (e.g., store, analyze, etc.)
+                    zipInputStream.closeEntry();
+                    sink.success();
                 }
-
-                zipInputStream.closeEntry();
+            } catch (IOException e) {
+                log.error("Failed to download data.", e);
+                sink.error( e);
             }
 
-            return Mono.empty();
-        } catch (IOException e) {
-            log.error("Failed to download data.", e);
-            return Mono.error(e);
-        }
+        }).subscribeOn(Schedulers.boundedElastic());
     }
 }
