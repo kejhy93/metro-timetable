076c9071c80ce8ef93e0487b7180f123cc515bf0
Lukáš Hejnal <kejhy93@gmail.com>
2026-03-25 00:19:20 +0100

    Refactor PIDClient to use constructor injection for RestClient

--- src/test/java/org/hejnaluk/metrotimetable/client/PIDClientTest.java
+++ src/test/java/org/hejnaluk/metrotimetable/client/PIDClientTest.java
@@ -6,7 +6,6 @@ import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.web.client.RestClient;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
