cc831f1bc675be97bed2144867caaa6c11f65282
Lukáš Hejnal <kejhy93@gmail.com>
2026-03-28 00:20:35 +0100

    Regular cron job to update cache

--- src/main/java/org/hejnaluk/metrotimetable/MetroTimetableApplication.java
+++ src/main/java/org/hejnaluk/metrotimetable/MetroTimetableApplication.java
@@ -2,8 +2,10 @@ package org.hejnaluk.metrotimetable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MetroTimetableApplication {
 
 	public static void main(String[] args) {
