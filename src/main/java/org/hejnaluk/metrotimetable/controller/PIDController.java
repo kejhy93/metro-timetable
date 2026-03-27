a34a9da7fd07806212053ee84946385c55f02623
Lukáš Hejnal <kejhy93@gmail.com>
2026-03-27 14:50:18 +0100

    Implement GET endpoint in controller

--- src/main/java/org/hejnaluk/metrotimetable/controller/PIDController.java
+++ src/main/java/org/hejnaluk/metrotimetable/controller/PIDController.java
@@ -4,11 +4,15 @@ package org.hejnaluk.metrotimetable.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hejnaluk.metrotimetable.client.PIDClient;
+import org.hejnaluk.metrotimetable.dto.TrainDeparture;
 import org.hejnaluk.metrotimetable.service.ParseTimetableService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pid")
@@ -27,6 +31,14 @@ public class PIDController {
         parseTimetableService.parseTimetableFiles();
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/station")
+    public ResponseEntity<List<TrainDeparture>> getTrainsForStation(
+            @RequestParam String station,
+            @RequestParam(required = false) Integer direction,
+            @RequestParam(defaultValue = "5") int limit) {
+        return ResponseEntity.ok(parseTimetableService.getTrainsForStation(station, direction, limit));
     }
 
 }
