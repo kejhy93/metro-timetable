ad2894d646f5f7b0e4689061910a0253982bbbf1
Lukáš Hejnal <kejhy93@gmail.com>
2026-03-30 20:38:35 +0200

    Fix deployment configuration and update Kotlin version (#86)

--- ui/composeApp/src/wasmJsMain/kotlin/org/hejnaluk/metrotimetable/ui/Platform.wasmJs.kt
+++ ui/composeApp/src/wasmJsMain/kotlin/org/hejnaluk/metrotimetable/ui/Platform.wasmJs.kt
@@ -2,12 +2,17 @@ package org.hejnaluk.metrotimetable.ui
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.js.Js
-import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
+@JsFun("() => Date.now()")
+private external fun dateNow(): Double
+
 actual fun currentLocalTime(): LocalTime =
-    Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).time
+    Instant.fromEpochMilliseconds(dateNow().toLong())
+        .toLocalDateTime(TimeZone.currentSystemDefault())
+        .time
 
 actual fun httpClientEngine(): HttpClientEngine = Js.create()
