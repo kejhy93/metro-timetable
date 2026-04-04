6c0a02edc4a824fcb74a6a6ccab5861dc275b18e
Lukáš Hejnal <kejhy93@gmail.com>
2026-04-04 20:22:37 +0200

    Fix gradle conflict

--- ui/composeApp/build.gradle.kts
+++ ui/composeApp/build.gradle.kts
@@ -26,7 +26,6 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
     alias(libs.plugins.kotlinxSerialization)
 }
 
