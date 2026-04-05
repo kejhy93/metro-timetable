28e2b2f105f9ac23f195157e9f8d0ec410a7d6b9
Lukáš Hejnal <kejhy93@gmail.com>
2026-04-05 07:46:13 +0200

    Client sends it's API version

--- ui/composeApp/src/commonMain/kotlin/org/hejnaluk/metrotimetable/ui/data/api/MetroApiClient.kt
+++ ui/composeApp/src/commonMain/kotlin/org/hejnaluk/metrotimetable/ui/data/api/MetroApiClient.kt
@@ -4,15 +4,18 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import org.hejnaluk.metrotimetable.ui.APP_VERSION
 
 class MetroApiClient(engine: HttpClientEngine, private val baseUrl: String) {
 
@@ -22,6 +25,9 @@ class MetroApiClient(engine: HttpClientEngine, private val baseUrl: String) {
         }
         install(Logging) {
             level = LogLevel.INFO
+        }
+        defaultRequest {
+            header("X-Client-Version", APP_VERSION)
         }
     }
 
